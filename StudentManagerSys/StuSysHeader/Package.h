b6e83152003db5b60386cb73f5e3fefc462a25ca
LSuperNova <LSuperNova@qq.com>
2022-11-24 20:06:57 +0800

    0.4

--- StudentManagerSys/StuSysHeader/Package.h
+++ StudentManagerSys/StuSysHeader/Package.h
@@ -10,68 +10,6 @@
 
 using namespace std;
 
-/* #ifndef _FUN_H
-#define _FUN_H
-
-// 使用正则表达式进行匹配，判断全局输入
-bool IsRegexInput(string str) 
-{
-	bool flag;
-	flag = true;
-	regex r("\\d{1,8}");
-	while (!(flag = regex_match(str, r)))
-	{
-		return flag;
-	}
-	return flag;
-
-}
-
-//显示或隐藏光标
-void ShowCursor(bool visible) { 
-
-    CONSOLE_CURSOR_INFO cursor_info = {20, visible};
-    //CONSOLE_CURSOR_INFO结构体包含控制台光标信息，成员分别表示光标百分比厚度和是否可见
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
- 	//SetConsoleCursorInfo设定控制台窗口的光标大小和是否可见
-}
-
-
-void SetTextRed()
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED | FOREGROUND_INTENSITY);
-}
-void SetTextGreen()
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN | FOREGROUND_INTENSITY);
-}
-
-void SetTextWhite()
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-}
-
-
-void gotoxy(int x,int y){  
-    COORD pos;  
-    pos.X=x;  
-    pos.Y=y;  
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),pos); 
-}
-
-void Mprintf(string str)
-{
-	cout<<setw(48)<<str;
-}
-void Cprintf(string str)
-{
-	cout<<setw(48+str.length()/2)<<str;
-}
-void SetBgWhite()
-{
-	
-}
-#endif */
 
 
 
